535df8de7bd0def80215a42e0ddbb346c0f321c5
chinki kumari singh <singhchinkikumari.com>
2022-06-17 22:28:53 +0530

    [Chinki] multipleRide

---
+++ CabInvoiceGenrator/CabInvoiceGenratorTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+using CabInvoiceGenerator;
+using NUnit.Framework;
+
+namespace CabInvoiceGenratorTest
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+
+        /// <summary>
+        /// Test case 1 Given the distance and time when invoice generator then should return total fare.
+        /// </summary>
+        [Test]
+        public void GivenDistanceAndTime_WhenInvoiceGenerator_ThenShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+        [Test]
+        public void GivenMultipleRides_WhenInvoiceGenerator_thenShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            InvoiceSummary invoiceSummary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+            Assert.AreEqual(expectedSummary, invoiceSummary);
+        }
+    }
+
+}
